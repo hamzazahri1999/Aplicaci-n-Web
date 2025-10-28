794627d36a9fa0b11b36f96378397785efff8af3
HAMZA ZAHRI <zahrihamza88@gmail.com>
2025-10-28 21:07:26 +0100

    Add ListView and DetailView for Cita model

--- citas/views.py
+++ citas/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Cita
 
-# Create your views here.
+class CitaListView(ListView):
+    model = Cita
+    template_name = 'citas/cita_list.html'
+    context_object_name = 'citas'
+
+class CitaDetailView(DetailView):
+    model = Cita
+    template_name = 'citas/cita_detail.html'
