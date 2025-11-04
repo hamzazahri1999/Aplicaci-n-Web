3204d1a74508da6f77ac1119d39699315d0fa5f0
HAMZA ZAHRI <zahrihamza88@gmail.com>
2025-11-04 20:21:59 +0100

    Add CRUD views for Cliente model

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Cliente
 
 # Vista para listar todos los clientes
@@ -12,3 +13,23 @@ class ClienteDetailView(DetailView):
     model = Cliente
     template_name = 'clientes/detalle_cliente.html'
     context_object_name = 'cliente'
+
+# Vista para crear un nuevo cliente
+class ClienteCreateView(CreateView):
+    model = Cliente
+    fields = ['nombre', 'telefono', 'email']
+    template_name = 'clientes/form_cliente.html'
+    success_url = reverse_lazy('lista_clientes')
+
+# Vista para actualizar un cliente existente
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    fields = ['nombre', 'telefono', 'email']
+    template_name = 'clientes/form_cliente.html'
+    success_url = reverse_lazy('lista_clientes')
+
+# Vista para borrar un cliente
+class ClienteDeleteView(DeleteView):
+    model = Cliente
+    template_name = 'clientes/confirmar_borrado.html'
+    success_url = reverse_lazy('lista_clientes')
