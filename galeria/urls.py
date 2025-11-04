d0d2c1dff81a15b001010871ed976fb452a9cfea
HAMZA ZAHRI <zahrihamza88@gmail.com>
2025-11-04 20:35:35 +0100

    Add CRUD paths for Foto views in urls.py

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,14 @@
+# galeria/urls.py
 from django.urls import path
-from .views import FotoListView, FotoDetailView
+from .views import (
+    FotoListView, FotoDetailView,
+    FotoCreateView, FotoUpdateView, FotoDeleteView
+)
 
 urlpatterns = [
     path('', FotoListView.as_view(), name='foto_list'),
     path('<int:pk>/', FotoDetailView.as_view(), name='foto_detail'),
+    path('crear/', FotoCreateView.as_view(), name='foto_create'),
+    path('<int:pk>/editar/', FotoUpdateView.as_view(), name='foto_update'),
+    path('<int:pk>/borrar/', FotoDeleteView.as_view(), name='foto_delete'),
 ]
