e909225f54558e8f074c4857133f60f048828851
HAMZA ZAHRI <zahrihamza88@gmail.com>
2025-11-04 20:23:28 +0100

    Add CRUD views for Servicio model

--- servicios/views.py
+++ servicios/views.py
@@ -1,11 +1,40 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Servicio
 
+
+# Listar servicios
 class ServicioListView(ListView):
     model = Servicio
     template_name = 'servicios/servicio_list.html'
     context_object_name = 'servicios'
 
+
+# Detalle de un servicio
 class ServicioDetailView(DetailView):
     model = Servicio
     template_name = 'servicios/servicio_detail.html'
+    context_object_name = 'servicio'
+
+
+# Crear servicio
+class ServicioCreateView(CreateView):
+    model = Servicio
+    template_name = 'servicios/servicio_form.html'
+    fields = ['nombre', 'precio']
+    success_url = reverse_lazy('servicio_list')
+
+
+# Editar servicio
+class ServicioUpdateView(UpdateView):
+    model = Servicio
+    template_name = 'servicios/servicio_form.html'
+    fields = ['nombre', 'precio']
+    success_url = reverse_lazy('servicio_list')
+
+
+# Eliminar servicio
+class ServicioDeleteView(DeleteView):
+    model = Servicio
+    template_name = 'servicios/servicio_confirm_delete.html'
+    success_url = reverse_lazy('servicio_list')
