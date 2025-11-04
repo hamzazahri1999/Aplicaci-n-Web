95dfe19568d27a1895ad236e9f39d2461dd5b052
HAMZA ZAHRI <zahrihamza88@gmail.com>
2025-11-04 20:21:04 +0100

    Add CRUD views for Cita model

--- citas/views.py
+++ citas/views.py
@@ -1,11 +1,35 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Cita
 
+# Lista de citas
 class CitaListView(ListView):
     model = Cita
     template_name = 'citas/cita_list.html'
     context_object_name = 'citas'
 
+# Detalle de una cita
 class CitaDetailView(DetailView):
     model = Cita
     template_name = 'citas/cita_detail.html'
+    context_object_name = 'cita'
+
+# Crear cita
+class CitaCreateView(CreateView):
+    model = Cita
+    template_name = 'citas/cita_form.html'
+    fields = ['cliente', 'servicio', 'fecha', 'hora']
+    success_url = reverse_lazy('cita_list')
+
+# Editar cita
+class CitaUpdateView(UpdateView):
+    model = Cita
+    template_name = 'citas/cita_form.html'
+    fields = ['cliente', 'servicio', 'fecha', 'hora']
+    success_url = reverse_lazy('cita_list')
+
+# Borrar cita
+class CitaDeleteView(DeleteView):
+    model = Cita
+    template_name = 'citas/cita_confirm_delete.html'
+    success_url = reverse_lazy('cita_list')
